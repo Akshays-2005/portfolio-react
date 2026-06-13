22d74693e625d3fb3561ba6103e6d17602807d3d
Akshay S <akshays.cs23@bmsce.ac.in>
2026-06-13 19:54:22 +0530

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -4,17 +4,32 @@ import Home from './Home';
 import About from './About';
 import Skills from './Skills';
 import Projects from './Projects';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
-  return (
-    <>
-      <NavBar />
-      <Home />
-      <About />
-      <Skills />
-      <Projects />
-    </>
-  );
-}
+    return (
+      <Router>
+        <div className="App">
+          <NavBar />
+          <div className="content">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/skills">
+                <Skills />
+              </Route>
+              <Route path="/projects">
+                <Projects />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 
 export default App;
