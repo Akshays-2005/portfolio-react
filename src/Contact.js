e0d53f4b71e636d6d9ed1833541f7289712ecbe5
Akshay S <akshays.cs23@bmsce.ac.in>
2026-08-18 12:44:22 +0530

    Update backend API URL for contact form

--- src/Contact.js
+++ src/Contact.js
@@ -26,7 +26,7 @@ const Contact = () => {
         setStatus('');
 
         try {
-            const response = await fetch('http://localhost:5000/send-email', {
+            const response = await fetch('https://portfolio-backend-u63q.onrender.com/send-email', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
