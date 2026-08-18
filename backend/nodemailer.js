5046c030323f91c15dafbc496f3500acc428c008
Akshay S <akshays.cs23@bmsce.ac.in>
2026-08-18 12:57:08 +0530

    Changed host

--- backend/nodemailer.js
+++ backend/nodemailer.js
@@ -10,10 +10,10 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 const transporter = nodemailer.createTransport({
-    // host:'smtp.gmail.com',
-    // port:465,
-    // secure:true,
-    service: 'gmail',
+    host:'smtp.gmail.com',
+    port:465,
+    secure:true,
+    // service: 'gmail',
     auth: {
         user: process.env.EMAIL,
         pass: process.env.PASSWORD
